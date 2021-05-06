fa014130f76644f0f5dd37c5feee8adff6febd07
TH0RlN <rarias-p@student.42madrid.com>
2021-05-06 17:59:21 +0200

    Nginx, PhpMyAd e Wordpress funcionando. Erra Mysql

--- setup.sh
+++ setup.sh
@@ -1,1 +1,23 @@
-.
+minikube delete
+
+minikube start --vm-driver=virtualbox
+
+eval $(minikube docker-env)
+
+minikube addons enable dashboard
+minikube addons enable metrics-server
+minikube addons enable metallb
+
+kubectl apply -f srcs/metallb.yaml
+
+docker build -t nginx srcs/nginx
+docker build -t wordpress srcs/wordpress
+docker build -t phpmyadmin srcs/phpmyadmin
+docker build -t mysql srcs/mysql
+
+kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/mysql.yaml
+
+minikube dashboard
