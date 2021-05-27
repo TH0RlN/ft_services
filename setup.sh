4ee3a81ff1984c9e74049750fd67780f5d8e6fcb
TH0RlN <rarias-p@student.42madrid.com>
2021-05-27 17:43:54 +0200

    Case listo

--- setup.sh
+++ setup.sh
@@ -47,8 +47,10 @@ kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/ftps.yaml
+kubectl apply -f srcs/grafana-config.yaml
 kubectl apply -f srcs/grafana.yaml
 kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/telegraf-config.yaml
 kubectl apply -f srcs/telegraf.yaml
 
 minikube dashboard
